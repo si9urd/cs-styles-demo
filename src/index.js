6164363951d379f2bf658f9e1f172e077090f829
si9urd <si9urd@gmail.com>
2024-10-05 16:44:28 +0300

    v0.0.7 header.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
+import format from "html-format"
+import beautify from 'simply-beautiful'
+import pretty from 'pretty'
 import hljs from './highlight/highlight.js'
 import './highlight/styles/github-dark-dimmed.css'
 import 'cs-styles/css/cs-styles.css'
 import './index.scss'
-
+import { setDefaultTheme } from './prefers-theme.js'
 document.addEventListener('DOMContentLoaded', function() {
+  setDefaultTheme()
   document.querySelectorAll('.code').forEach(function(element) {
+    console.log('>>', format(element.innerHTML, ' '.repeat(4), 120))
     const codeElement = document.createElement('div')
-    codeElement.innerHTML = `<pre><code class="language-html">${element.innerHTML}</code></pre>`
+    codeElement.innerHTML = `<pre><code class="language-html">${pretty(element.innerHTML, { ocd: true })}</code></pre>`
+    // codeElement.innerHTML = `<pre><code class="language-html">${beautify.html(element.innerHTML)}</code></pre>`
     element.after(codeElement)
   })
   document.querySelectorAll('code').forEach(function(element) {
     element.innerHTML = element.innerHTML.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#039;')
   })
   hljs.highlightAll();
-  // toggleTheme()
 })
-//
-// function toggleTheme () {
-//   const theme = document.documentElement.getAttribute('data-theme')
-//   document.documentElement.setAttribute('data-theme', theme === 'light' ? 'dark' : 'light')
-// }
