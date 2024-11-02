00888d2a1031ad5be92c755f494afab958fbb71a
si9urd <si9urd@gmail.com>
2024-11-02 14:02:29 +0200

    v0.7.5 added elevation.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import format from "html-format"
 import pretty from 'pretty'
 import hljs from './highlight/highlight.js'
 import './highlight/github.scss'
@@ -10,10 +9,8 @@ import { setDefaultTheme } from './prefers-theme.js'
 document.addEventListener('DOMContentLoaded', function() {
   setDefaultTheme()
   document.querySelectorAll('.code').forEach(function(element) {
-    console.log('>>', format(element.innerHTML, ' '.repeat(4), 120))
     const codeElement = document.createElement('div')
     codeElement.innerHTML = `<pre><code class="language-html">${pretty(element.innerHTML, { ocd: true })}</code></pre>`
-    // codeElement.innerHTML = `<pre><code class="language-html">${beautify.html(element.innerHTML)}</code></pre>`
     element.after(codeElement)
   })
   document.querySelectorAll('code').forEach(function(element) {
