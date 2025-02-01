45cbb9f5175e3c439a7a046a9041c00131c1b200
si9urd <si9urd@gmail.com>
2025-02-01 14:50:17 +0200

    v0.7.29

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { setDefaultTheme } from './js/prefers-theme.js'
 import { setScrollSooth } from './js/scroll-smooth.js'
 import { generateColorSchemeHtml } from './js/colors-scheme.js'
 import { setCodeHighlight } from './js/code-highlight.js'
-import 'cs-styles/dist/cs-styles.css'
+// import 'cs-styles/dist/cs-styles.css'
 import './scss/index.scss'
 
 function initialize() {
